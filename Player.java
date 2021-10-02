f9e243f3184186f8967d11931beddbabded7742b
MarkWholihan <forensicgeneticgenealogy@gmail.com>
2021-10-01 21:44:02 -0400

    Update Player.java
    updated Player Class some more

--- Player.java
+++ Player.java
@@ -14,69 +14,76 @@ public class Player {
 	private ArrayList<Chance> cards;
 	private ArrayList<Property> ownedProperties;
 	public static final int spaceTotal = 24;
-	
-	// creates an object of a player
+
+	// constructs an object of player
 	public Player(String name) {
-		name = this.name;
+		this.name = name;
 		cash = 16;
 		location = 0;
 		getOutOfJailCard = false;
 		cards = new ArrayList<Chance>();
 		ownedProperties = new ArrayList<Property>();
 	}
+
+	//  returns information about this player as a string
+	public String toString() {
+		String result = ("Name: " + name + "\nCash: " + cash + "\nLocation: " + location + "\nHas get out of jail free card? " + getOutOfJailCard + "\nProperties owned: " + ownedProperties);
+
+		return result;
+	}
 	
 	// returns players name
 	public String getName() {
-		return name;
+		return this.name;
 	}
-	
+
 	// moves players location
 	public int move(int spaces) {
 		location = (spaces + location) % spaceTotal;
 		return location;
 	}
-	
+
 	// returns a players location
 	public int getLoc() {
 		return location;
 	}
-	
+
 	// sets a players location
 	public void setLoc(int location) {
 		location = this.location;
 	}
-	
+
 	// returns players cash amount
 	public int getCash() {
 		return cash;
 	}
-	
+
 	// adds cash to the player
 	public void addCash(int cash) {
 		cash += this.cash;
 	}
-	
+
 	// subtracts cash from the player
 	public void subtractCash(int cash) {
 		cash -= this.cash;
 	}
-	
+
 	// pays another player
-	//public void payPlayer(Player pay, int amount) {
-	//	payPlayer(amount);
-	//	pay.addCash(amount);
-	//}
-	
+	public void payPlayer(Player payee, int amount) {
+		subtractCash(amount);
+		payee.addCash(amount);
+	}
+
 	// adds a property to the player
 	public void addProperty(Property property) {
 		ownedProperties.add(property);
 	}
-	
+
 	// returns players properties
 	public ArrayList<Property> getProperties() {
 		return ownedProperties;
 	}
-	
+
 	// returns true if player has get out of jail free card
 	public Boolean getOutOfJailFreeCard() {
 		if(cards.size() > 0) {
@@ -89,13 +96,13 @@ public class Player {
 			return false;
 		}
 	}
-	
+
 	// uses getOutOfJailCard
 	public void useCard(Chance goojf) {
 		cards.add(goojf);
 		getOutOfJailCard = true;
 	}
-	
+
 	// player is in jail
 	public Boolean playerInJail() {
 		return inJail;
