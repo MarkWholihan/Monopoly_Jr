7e370ecf112ddf50fa87aefc958c44e7d8986cf8
MarkWholihan <forensicgeneticgenealogy@gmail.com>
2021-10-01 21:44:41 -0400

    Update MonopolyJr.java
    Testing out creating instances of players and printing Player information

--- MonopolyJr.java
+++ MonopolyJr.java
@@ -1,3 +1,4 @@
+
 /*  Programmers: Mark Wholihan & ________
 Course: COSC 211, Fall 2021
 Project#: 1 Monopoly Jr
@@ -6,6 +7,14 @@ Due date: 10-10-2021 by 11:59pm
 
 public class MonopolyJr {
 	public static void main(String [] args) {
-		System.out.println("This is the class where we execute the program");
+		System.out.println("This is the starting players information\n");
+		
+		Player playerOne = new Player("Toy Boat");
+		Player playerTwo = new Player("Toy Car");
+		Player playerThree = new Player("Little Hazel");
+		Player playerFour = new Player("Little Scottie");
+		
+		System.out.println(playerOne + " \n\n" + playerTwo + " \n\n" + playerThree + " \n\n" + playerFour);
+		System.out.println();
 	}
 }
