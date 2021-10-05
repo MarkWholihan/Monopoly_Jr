1a6d1d3341b033a2104ea490e3d94b27f53bedfd
MarkWholihan <forensicgeneticgenealogy@gmail.com>
2021-10-05 11:55:14 -0400

    Update MonopolyJr.java
    made an ArrayList of players

--- MonopolyJr.java
+++ MonopolyJr.java
@@ -4,7 +4,7 @@ Course: COSC 211, Fall 2021
 Project#: 1 Monopoly Jr
 Due date: 10-10-2021 by 11:59pm
  */
-
+import java.util.*;
 public class MonopolyJr {
 	public static void main(String [] args) {
 		System.out.println("This is the starting players information\n");
@@ -13,6 +13,13 @@ public class MonopolyJr {
 		Player playerTwo = new Player("Toy Car");
 		Player playerThree = new Player("Little Hazel");
 		Player playerFour = new Player("Little Scottie");
+		
+		ArrayList<Player> players = new ArrayList<Player>();
+		players.add(playerOne);
+		players.add(playerTwo);
+		players.add(playerThree);
+		players.add(playerFour);
+		
 		
 		System.out.println(playerOne + " \n\n" + playerTwo + " \n\n" + playerThree + " \n\n" + playerFour);
 		System.out.println();
