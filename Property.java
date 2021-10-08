9715788305c56bef011939ca74ecf5105f0238ff
MarkWholihan <forensicgeneticgenealogy@gmail.com>
2021-10-08 17:53:48 -0400

    onLanding method
    unowned property Arraylist
    and a series of nested if statements for adding and subtracting cash and properties

--- Property.java
+++ Property.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 /*  Programmers: Mark Wholihan & Alder Yang
 Course: COSC 211, Fall 2021
 Project#: 1 Monopoly Jr
@@ -34,22 +36,41 @@ public class Property extends BoardSpace {
 		return owner;
 	}
 
-
-
 	// do things when player lands on this space
 	public void OnLanding(Player currentPlayer, Property currentProperty) {
+		// ArrayList of Property objects
+		ArrayList<Property> unownedProperties = new ArrayList<>();
+		unownedProperties.add(GameBoard.Chicfila);
+		unownedProperties.add(GameBoard.LittleCaesars);
+		unownedProperties.add(GameBoard.DairyQueen);
+		unownedProperties.add(GameBoard.ChocoFactory);
+		unownedProperties.add(GameBoard.HalleLibrary);
+		unownedProperties.add(GameBoard.YpsiMuseum);
+		unownedProperties.add(GameBoard.FrogIslandPark);
+		unownedProperties.add(GameBoard.FordLake);
+		unownedProperties.add(GameBoard.PinballPetes);
+		unownedProperties.add(GameBoard.MJR);
+		unownedProperties.add(GameBoard.Petco);
+		unownedProperties.add(GameBoard.ToysRUs);
+		unownedProperties.add(GameBoard.Bowlero);
+		unownedProperties.add(GameBoard.DetroitZoo);
+		unownedProperties.add(GameBoard.Starbucks);
+		unownedProperties.add(GameBoard.AppleStore);
 		//update the player’s cash balance,
-
 		//update the player’s position on the board, and
-
-
 		//if class is a property, update the state of the property (owned/unowned, and owner).
-		if (currentProperty == GameBoard.Chicfila) {	
-
-			if (owned == true) {
-				currentPlayer.payPlayer(owner, cost);
+		for (int i = 0; i < unownedProperties.size(); i++) {
+			if (currentProperty == unownedProperties.get(i)) {
+				if (owned == true) {
+					currentPlayer.payPlayer(owner, cost);
+				} else {
+					currentPlayer.addProperty(currentProperty);
+					unownedProperties.remove(i);
+					owned = true;
+				}
 			} else {
-				currentPlayer.addProperty(currentProperty);
+				System.out.println("GameOVER");
+				System.exit(0);
 			}
 		}
 	}
