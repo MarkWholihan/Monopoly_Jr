e048984c427c0282ddd706c07b31eb74e5fe0509
MarkWholihan <forensicgeneticgenealogy@gmail.com>
2021-10-17 16:16:55 -0400

    Update Property.java
    OnLanding method now working

--- Property.java
+++ Property.java
@@ -12,8 +12,8 @@ public class Property extends BoardSpace {
 	private String name;
 	private int cost;
 	private String color;
-	private boolean owned = false;
-	private Player owner = Player.banker;
+	private boolean owned;
+	private Player owner;
 
 	public Property(String pName, int pCost, String pColor) {
 		super("Property");
@@ -26,12 +26,17 @@ public class Property extends BoardSpace {
 
 	// returns information about this player as a string
 	public String toString() {
-		String result = ("\nName: " + name + "\nCost: " + cost + "\nColor: "
+		String result = ("Name: " + name + "\nCost: " + cost + "\nColor: "
 				+ color + "\nIs owned? "
 				+ owned + "\nOwner: "
 				+ owner.getName());
 		return result;
 	}
+	
+	// returns a locations name
+		public String getLocName() {
+			return name;
+		}
 
 	public boolean Status() {
 		return owned;
@@ -44,46 +49,20 @@ public class Property extends BoardSpace {
 
 	// do things when player lands on this space
 	public static void OnLanding(Player currentPlayer, Property currentProperty) {
-
-		// ArrayList of Property objects
-		ArrayList<Property> unownedProperties = new ArrayList<>();
-		unownedProperties.add(GameBoard.Chicfila);
-		unownedProperties.add(GameBoard.LittleCaesars);
-		unownedProperties.add(GameBoard.DairyQueen);
-		unownedProperties.add(GameBoard.ChocoFactory);
-		unownedProperties.add(GameBoard.HalleLibrary);
-		unownedProperties.add(GameBoard.YpsiMuseum);
-		unownedProperties.add(GameBoard.FrogIslandPark);
-		unownedProperties.add(GameBoard.FordLake);
-		unownedProperties.add(GameBoard.PinballPetes);
-		unownedProperties.add(GameBoard.MJR);
-		unownedProperties.add(GameBoard.Petco);
-		unownedProperties.add(GameBoard.ToysRUs);
-		unownedProperties.add(GameBoard.Bowlero);
-		unownedProperties.add(GameBoard.DetroitZoo);
-		unownedProperties.add(GameBoard.Starbucks);
-		unownedProperties.add(GameBoard.AppleStore);
-
 		//update the player’s cash balance,
 		//update the player’s position on the board, and
 		//if class is a property, update the state of the property (owned/unowned, and owner).
-		for (int i = 0; i < unownedProperties.size(); i++) {
-			if (currentProperty.equals(unownedProperties.get(i))) {
-				System.out.println("This property is unowned!" + unownedProperties.get(i));
-
-				if (currentProperty.owned == true) {
-					currentPlayer.payPlayer(currentProperty.owner, currentProperty.cost);
-					System.out.println("Paid " + currentProperty.owner.getName() + " $" + currentProperty.cost + " for rent");
-				} else {
-					currentPlayer.addProperty(currentProperty);
-					unownedProperties.remove(i);
-					currentProperty.owned = true;
-					currentProperty.owner = currentPlayer;
-					System.out.println("Property purchased for $" + currentProperty.cost );
-					System.out.println("Property added to players properties");
-					break;
-				}
-			}  
+		if (currentProperty.owned == true) {
+			System.out.println();
+			currentPlayer.payPlayer(currentProperty.owner, currentProperty.cost);
+			System.out.println(currentPlayer.getName() + " paid " + currentProperty.owner.getName() + " $" + currentProperty.cost + " for rent");
+		} else {
+			System.out.println();
+			currentPlayer.addProperty(currentProperty);
+			currentProperty.owned = true;
+			currentProperty.owner = currentPlayer;
+			System.out.println(currentProperty.name + " purchased for $" + currentProperty.cost + " by " + currentPlayer.getName());
+			System.out.println(currentProperty.name + " property added to " + currentPlayer.getName() + "'s properties");
 		}
 	}
 }
